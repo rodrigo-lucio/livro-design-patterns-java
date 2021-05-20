4ae1b84b81e55fd9d4470a5914dbf3dd5defd01b
Rodrigo Lúcio <56269484+rodrigo-lucio@users.noreply.github.com>
2021-05-19 22:18:29 -0300

    Estudo de caso do padrão Template Method

---
+++ src/br/com/lucio/templatemethod/GeradorArquivo.java
@@ -0,0 +1,23 @@
+package br.com.lucio.templatemethod;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Map;
+
+public abstract class GeradorArquivo {
+	public final void gerarArquivo(String nome, Map<String, Object> propriedades) throws IOException {
+		String conteudo = gerarConteudo(propriedades);
+		byte[] bytes = conteudo.getBytes();
+		bytes = processar(bytes);
+		FileOutputStream fileout = new FileOutputStream(nome);
+		fileout.write(bytes);
+		fileout.close();
+	}
+
+	protected byte[] processar(byte[] bytes) throws IOException {
+		return bytes;
+	}
+
+	protected abstract String gerarConteudo(Map<String, Object> propriedades);
+
+}
