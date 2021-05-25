ea8d4f04c56f3be1d52b666f5e45bb7fbc998453
Rodrigo Lúcio <56269484+rodrigo-lucio@users.noreply.github.com>
2021-05-24 22:02:46 -0300

    Estudo de caso do padrão State

---
+++ src/br/com/lucio/state/Cor.java
@@ -0,0 +1,12 @@
+package br.com.lucio.state;
+
+import java.util.List;
+
+public abstract class Cor {
+	void busca(No no, List<No> list) {
+	}
+
+	void assumiu(No no, List<No> list) {
+	}
+}
+//Implementações
