2ee979d9d4ef8522913bb5fc29f096e45bc5bbae
Rodrigo Lúcio <56269484+rodrigo-lucio@users.noreply.github.com>
2021-05-26 22:32:07 -0300

    Metodo main para o teste do observer

---
+++ src/br/com/lucio/observer/Main.java
@@ -0,0 +1,22 @@
+package br.com.lucio.observer;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		GraficoBarras gb = new GraficoBarras();
+		AcoesLogger al = new AcoesLogger();
+		CarteiraAcoes c = new CarteiraAcoes();
+		c.addObservador(gb);
+		c.addObservador(al);
+		Thread.sleep(2000);
+		c.adicionaAcoes("COMP02", 200);
+		Thread.sleep(2000);
+		c.adicionaAcoes("EMP34", 400);
+		Thread.sleep(2000);
+		c.adicionaAcoes("ACDF89", 300);
+		Thread.sleep(2000);
+		c.adicionaAcoes("EMP34", -200);
+		Thread.sleep(2000);
+		c.adicionaAcoes("COMP02", 150);
+	}
+}
